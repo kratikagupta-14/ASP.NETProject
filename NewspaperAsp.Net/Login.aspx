cd6858348427eeee6bdeafcc046882c42e14abd1
kratika Gupta <kratikagupta1412@hotmail.com>
2020-05-14 17:58:58 +0530

    first commit

---
+++ NewspaperAsp.Net/Login.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="NewspaperAsp.Net.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="Content/css/bootstrap.css" rel="stylesheet" />
+    <link href="Content/css/SignIn.css" rel="stylesheet" />
+    <style>
+       body {
+    background-image: url('~/Content/img/login-background.png');
+}
+    </style>
+</head>
+<body>
+    <div class="container">
+        <form class="form-signin" runat="server">
+            <h3 class="alert alert-success">Newspaper Agency</h3>
+
+            <label for="inputEmail" class="sr-only">User Name</label>
+            <input type="text" id="inputEmail" class="form-control" placeholder="User Name" runat="server"
+                required autofocus />
+
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" runat="server" id="inputPassword" class="form-control" placeholder="Password"
+                required />
+
+            <asp:Button runat="server" CssClass="btn btn-lg btn-success btn-block" 
+                Text="Sign In" ID="btnLogin" OnClick="btnLogin_Click" />
+        </form>
+
+    </div>
+</body>
+</html>
---
+++ NewspaperAsp.Net/Login.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.IO.Pipes;
+using System.Web.UI.WebControls.WebParts;
+using System.Data.SqlClient;
+
+namespace NewspaperAsp.Net
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (inputEmail.Value.ToUpper() == "ADMIN" && inputPassword.Value.ToUpper() =="ADMIN")
+            {
+                Response.Redirect("Home.aspx");
+            }
+            /*
+            else
+            {
+                LoginDetails log = ValidateUser(username.Value, psw.Value);
+
+                if(log.IsAuthUser)
+                {
+                    Session["UserID"] = log.UserId;
+                    Session["role"] = log.role;
+                    Session["username"] = log.username;
+                    Session["IsAuth"] = log.IsAuthUser;
+                    Response.Redirect("Home.aspx");
+
+                }
+                else
+                {
+                    Response.Redirect("Login.aspx");
+                }
+
+            }
+        }
+
+        private LoginDetails ValidateUser(string username, string psw)
+        {
+            LoginDetails obj = new LoginDeatils();
+            obj.IsAuthUser = false;
+
+            SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["bs"].ConnectionString);
+            SqlDataAdapter da;
+            DataSet ds = new DataSet();
+            string qr = sele 
+            */
+
+        }  
+
+    }
+}
